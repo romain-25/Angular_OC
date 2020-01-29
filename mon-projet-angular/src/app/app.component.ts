027ae97e4138017d19e24f63ec4f9ea9984b6303
Romain-25 <romain.rouabah@gmail.com>
2020-01-29 22:47:05 +0100

    routing

--- mon-projet-angular/src/app/app.component.ts
+++ mon-projet-angular/src/app/app.component.ts
@@ -6,36 +6,9 @@ import { AppareilService } from "./services/appareil.service";
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent implements OnInit{
-  isAuth = false;
-  lastUpdate = new Promise((resolve, reject) => {
-    const date = new Date();
-    setTimeout(
-      () => {
-        resolve(date);
-      }, 2000
-    );
-  });
-
-appareils: any[];
-
-
-  constructor(private appareilService: AppareilService){
-    setTimeout(
-      () => {
-        this.isAuth = true;
-      }, 4000
-    );
+export class AppComponent {
+  constructor(){
+    
   }
-
-  ngOnInit(){
-    this.appareils = this.appareilService.appareils;
-  }
-  onAllumer(){
-    this.appareilService.switchOnAll();
-  }
-
-  onEteindre(){
-    this.appareilService.switchOffAll();
-  }
+  
 }
