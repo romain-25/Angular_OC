5f80d1f6ce151682d7442f29b1d674dc1eda8a63
Romain-25 <romain.rouabah@gmail.com>
2020-02-02 17:59:19 +0100

    reactive

--- mon-projet-angular/src/app/services/user.service.ts
+++ mon-projet-angular/src/app/services/user.service.ts
@@ -2,7 +2,18 @@ import { User } from '../models/User.model';
 import { Subject } from 'rxjs';
 
 export class UserService {
-    private users: User[] = [];
+    private users: User[] = [
+        {
+            firstName: 'James',
+            lastName: 'Smith',
+            email: 'james@smith.com',
+            drinkPreference: 'Coca',
+            hobbies: [
+                'coder',
+                'la dégustation de café'
+            ]
+        }
+    ];
     userSubject = new Subject<User[]>();
 
     emitUsers() {
